7d2b5c4be82b4c8ade65a1811f88bf8f2907ea0c
t-k- <clock126@126.com>
2018-07-26 17:53:05 -0400

    added tab to drawer.

--- qryBar.vue
+++ qryBar.vue
@@ -10,27 +10,35 @@
       @click:append="test('math pad')"
       clearable>
     </v-text-field>
-    <v-btn color="info" @click.stop="drawer_toggle"><v-icon>search</v-icon></v-btn>
+    <v-btn color="info" @click.stop="drawer_set(true)"><v-icon>search</v-icon></v-btn>
   </v-layout>
 </div>
 <div style="position: relative; height: 100%">
   <img style="width: 100%; max-height: 700px" src="https://www.iwantwallpaper.co.uk/images/muriva-bluff-embossed-brick-effect-wallpaper-j30309-p711-1303_image.jpg"/>
   <v-container fluid>
+
     <v-navigation-drawer v-model="drawer" width="800"
      right stateless temporary absolute>
-    <v-container>
-    <ul>
-      <li v-for="hit in hits">
-      <p class="sni-title"> {{hit.title}}</p>
-      <p class="sni-url">{{hit.url}}     </p>
-      <p class="snippet">{{hit.snippet}} </p>
-      </li>
-    </ul>
-    <v-layout justify-center>
-      <v-pagination v-model="page" :length="20" :total-visible="7"></v-pagination>
-    </v-layout>
-    </v-container>
+<div class="tab" @click="drawer_set(!drawer)" style="left: -25px">
+<v-icon v-if="!drawer">navigate_before</v-icon>
+<v-icon v-else>navigate_next</v-icon>
+</div>
+    <div style="height: 100%; overflow: auto;">
+      <v-container>
+      <ul>
+        <li v-for="hit in hits">
+        <p class="sni-title"> {{hit.title}}</p>
+        <p class="sni-url">{{hit.url}}     </p>
+        <p class="snippet">{{hit.snippet}} </p>
+        </li>
+      </ul>
+      <v-layout justify-center>
+        <v-pagination v-model="page" :length="20" :total-visible="7"></v-pagination>
+      </v-layout>
+      </v-container>
+    </div>
     </v-navigation-drawer>
+
   </v-container>
 </div>
 </v-app>
@@ -57,31 +65,42 @@ export default {
     test: function (str) {
       console.log('test' + str);
     },
-    drawer_toggle () {
-      this.drawer = !this.drawer;
+    drawer_set(state) {
+      this.drawer = state;
       var vm = this;
       setTimeout(function () {
+        $('.v-overlay').unbind('click');
         $('.v-overlay').click(function () {
-          vm.drawer = !vm.drawer;
+          vm.drawer = false;
         })
       }, 500);
     },
   }
 }
 /*
-  <div id="sidebar-parent" style="position: relative;">
-    <vs-sidebar :vs-active.sync="active" vs-parent="#sidebar-parent">
-    </vs-sidebar>
-  </div>
 */
 </script>
 
 <style>
 .sni-title {
-	font-size: 1.2em;
-	color: blue;
+  font-size: 1.2em;
+  color: blue;
 }
 .sni-url {
-	color: green;
+  color: green;
+}
+
+.v-navigation-drawer {
+  overflow-x: visible !important;
+  overflow-y: visible !important;
+}
+div.tab {
+  position: absolute;
+  top: 50px;
+  width: 25px;
+  height: 50px;
+  background-color: white;
+  padding-top: 12px;
+  cursor: pointer;
 }
 </style>
