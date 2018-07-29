a890e8d2a515065deaa74bb65b31ca6f7a6cf3aa
t-k- <clock126@126.com>
2018-07-29 07:09:55 -0400

    fix paths.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               outputPath: 'fonts/',
-              publicPath: 'dist/fonts/'
+              publicPath: 'fonts/'
             }
           }]
         }
